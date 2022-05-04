6bb303a3810e095cf5b42bbba5b8fe2a7c328c33
NIKNIK67 <56790565+NIKNIK67@users.noreply.github.com>
2022-05-04 10:24:13 +0200

    HospitalAppStart

---
+++ HospitalApp/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HospitalApp
+{
+    public  class Form1 : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+    }
+}
